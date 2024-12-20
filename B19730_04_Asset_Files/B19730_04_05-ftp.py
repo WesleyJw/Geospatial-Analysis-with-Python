c29228b8ddcd604c5666319263e240c24764187d
WesleyJw <jwesleybiologo@gmail.com>
2024-12-20 19:11:57 -0300

    studying chapter for - get data from web

--- B19730_04_Asset_Files/B19730_04_05-ftp.py
+++ B19730_04_Asset_Files/B19730_04_05-ftp.py
@@ -1,20 +1,27 @@
+# %%
 # Read NOAA weather buoy location data via ftp
 import ftplib
+
+# %%
 server = "ftp.pmel.noaa.gov"
 dir = "taodata"
 fileName = "taobuoypos.dat"
-ftp = ftplib.FTP(server)
+ftp = ftplib.FTP_TLS(server)
 ftp.login()
 ftp.cwd(dir)
-with open(fileName, "wb") as out:
+ftp.prot_p()
+
+with open("../data/"+fileName, "wb") as out:
     ftp.retrbinary(f"RETR {fileName}", out.write)
     ftp.quit()
 
-with open(fileName) as tao:
+# %%
+with open("../data/"+fileName) as tao:
     buoy = tao.readlines()[5]
     loc = buoy.split()
     print(f"Buoy {loc[0]} is located at {' '.join(loc[4:8])}")
 
+# %%
 # Now do the same thing with urllib
 
 import urllib.request
@@ -23,3 +30,4 @@ tao = urllib.request.urlopen(f"ftp://{server}/{dir}/{fileName}")
 buoy = str(tao.readlines()[5], encoding="utf8")
 loc = buoy.split()
 print(f"Buoy {loc[0]} is located at {' '.join(loc[4:8])}")
+# %%
